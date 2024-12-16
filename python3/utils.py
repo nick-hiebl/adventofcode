f1ca14fd2497caaeca21483cfac5c11435075a28
Nicholas Hiebl <nicholas@hiebl.net.au>
2024-12-16 15:19:59 +1100

    Add Chinese remainder theorem functions

--- python3/utils.py
+++ python3/utils.py
@@ -1,6 +1,7 @@
 import sys
 from collections import defaultdict, deque
 from queue import PriorityQueue
+from functools import reduce
 
 def readFile():
   with open(sys.argv[1], 'r') as f:
@@ -450,3 +451,24 @@ def test(name, expected, actual):
     print('FAIL! {} expected: {}, but got: {}'.format(name, expected, actual))
     print()
     assert expected == actual
+
+# chinese_remainder([101, 103], [14, 94]) == 6377
+
+def chinese_remainder(n, a):
+  sum = 0
+  prod = reduce(lambda a, b: a*b, n)
+  for n_i, a_i in zip(n, a):
+    p = prod // n_i
+    sum += a_i * mul_inv(p, n_i) * p
+  return sum % prod
+
+def mul_inv(a, b):
+  b0 = b
+  x0, x1 = 0, 1
+  if b == 1: return 1
+  while a > 1:
+    q = a // b
+    a, b = b, a%b
+    x0, x1 = x1 - q * x0, x0
+  if x1 < 0: x1 += b0
+  return x1
