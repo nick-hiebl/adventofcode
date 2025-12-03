220a52b6b89708d4eadb1348bd9b9f29896b0e10
Nicholas Hiebl <nicholas@hiebl.net.au>
2025-12-03 18:21:47 +1100

    Simplified code

--- y2025/day03/main.py
+++ y2025/day03/main.py
@@ -1,6 +1,4 @@
 from utils import *
-import re
-from functools import cache
 
 def processInput(data):
   out = []
@@ -10,36 +8,13 @@ def processInput(data):
 
   return out
 
-my_map = {}
-
-def best_bank_score(key, bank, num_to_choose_from, prior):
-  # print('called with', bank, num_to_choose_from, prior)
-  if num_to_choose_from == 0:
-    return prior
-  elif num_to_choose_from > len(bank):
-    return 0
-
-  my_key = (key, num_to_choose_from)
-
-  if my_key in my_map:
-    if prior < my_map[my_key]:
-      return 0
-
-  my_map[my_key] = prior
-
-  a = best_bank_score(key, bank[1:], num_to_choose_from, prior)
-  b = best_bank_score(key, bank[1:], num_to_choose_from - 1, prior * 10 + bank[0])
-
-  return max(a,b)
-
-def best_bank_score2(bank, length):
+def best_bank_score(bank, length, prior = 0):
   if length == 0:
-    return 0
+    return prior
 
   first_digit = max(bank[0:len(bank) - length + 1])
-  base = first_digit * (10 ** (length - 1))
-  return base + best_bank_score2(bank[bank.index(first_digit) + 1:], length - 1)
-
+  remaining_digits = bank[bank.index(first_digit) + 1:]
+  return best_bank_score(remaining_digits, length - 1, prior * 10 + first_digit)
 
 def main(raw, part):
   total = 0
@@ -48,31 +23,12 @@ def main(raw, part):
 
   if part == 1:
     for bank in data:
-      v = best_bank_score2(bank, 2)
-      total += v
-      # best = 0
-      # for i, left in enumerate(bank):
-      #   if i == len(bank) - 1:
-      #     continue
-      #   for right in bank[i+1:]:
-      #     best = max(best, 10 * left + right)
-      
-      # total += best
+      total += best_bank_score(bank, 2)
     return total
   elif part == 2:
     global my_map
     for key, bank in enumerate(data):
-      v = best_bank_score2(bank, 12)
-      # print(v)
-      total += v
-      # v = best_bank_score(key, bank, 12, 0)
-      # print(v)
-
-      # if v == 0:
-      #   print(my_map)
-
-      # total += v
-      # my_map = {}
+      total += best_bank_score(bank, 12)
 
     return total
 
